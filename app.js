0de88085192d90fa8f8513b5b14a4ffb76316500
Syed Farook <syedfarook143@gmail.com>
2024-03-20 13:58:19 +0530

    javacommit

---
+++ app.js
@@ -0,0 +1,146 @@
+ mod=10%3;
+
+// // // pre and posr incre
+
+// // let a=10;
+// // let newa=a++;
+// // let newb=++a;
+
+// // // strings
+
+// // let name="farooq";
+// // console.log(name.length,name[0]);
+
+
+// // let fanme="syed";
+// // let lname="farooq";
+// // let fullname = fanme + lname;
+// // console.log(fullname);
+
+// // // asignment
+
+// // let name1="farooq";
+// // console.log((name1.length),name1[0],name1[name1.length-1]);
+
+// // let n="farooq";
+// // console.log(`my name is ${n}`);
+
+
+// // // operators
+
+// // let age=8;
+
+// // console.log(age==age);
+
+// // // condtional statments
+
+// // let input="green"
+// // let color1="red";
+// // let color2="org";
+// // let color3="green";
+
+// // if(input==color1){
+// //     console.log("stop");
+// // }
+// // if(input==color2){
+// //     console.log("wait");
+// // }
+// // if(input==color3){
+// //     console.log("Go");
+// // }
+
+// // // -------------------------------
+// // let size="";
+// // if(size=="xl"){
+// //     console.log("price 250");}
+
+// // else if(size=="l"){
+// //     console.log("price 200");
+// // }
+
+// // else if(size=="m"){
+// //     console.log("price 100");
+// // }
+// // else{
+// //     console.log("50")
+// // }
+// // // ----------nested if&else------
+
+// let mar=34;
+
+
+
+// if(mar>=35)
+//     if(mar>=80){
+//         console.log("a")
+//     } 
+//     else{
+//         console.log("b")
+//     }
+// }
+// else{
+//     console.log("fail")
+// }
+// -----------------------
+// if((mar>=35)&&(mar>=80)){
+//    console.log("super") 
+// }
+// else if( (mar>=50)&&(mar>=35)){
+//     console.log("ok")
+// }
+// else{
+//     console.log("waste")
+// }
+
+
+// ----------------
+// let str="apple";
+// if((str.length>3)&&(str[0]='a')){
+//     console.log("Good string");
+// }
+
+// let name=prompt("enter your name");
+// console.log(name);
+
+// alert("hey evvvara meru");
+
+// ----------
+
+// 
+// string methods
+
+
+// let string="syedomarfarooq";
+// console.log(string.trim().toUpperCase());
+
+
+// console.log(string.repeat(3,"syed"));
+
+// let mess="help";
+
+// console.log(mess.trim().toUpperCase());
+
+
+// let name=["syed","farooq","omar"];
+
+
+// name.push("fayaz");
+// name.pop();
+
+// let name1=['jan','jul','mar','aug'];
+
+
+// for(i=1;i<=5;i++){
+//     for(j=1;j<=5;j++){
+//         console.log(j);
+//     }
+// }
+
+
+
+for(i=1;i<=5;i++){
+    console.log(i);
+    if(i==3){
+        break;
+    }
+}
