e0d73a3c1287a8fb738ff594b69e0d42196e2381
Syed Farook <syedfarook143@gmail.com>
2024-03-23 05:50:13 +0530

    javascriptRevision

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
- mod=10%3;
+//  mod=10%3;
 
 // // // pre and posr incre
 
@@ -136,11 +136,285 @@
 //     }
 // }
 
+// for (let i=0;i<=50;i++){
+//     console.log(i);
+// }
 
 
-for(i=1;i<=5;i++){
-    console.log(i);
-    if(i==3){
-        break;
+// for(let i=1;i<=3;i++){
+//     for (j=0;j<=5;j++){
+//         console.log(j);
+//     }
+// }
+
+
+// let i=1;
+// while(i<=5){
+//     console.log(i);
+
+// i++}
+
+
+// const place="hyderabad";
+// let guss=prompt("Guss rey");
+
+// while(true){
+//     if (place==guss){console.log("good guss");
+//     break;
+// }
+// if(place!==guss){
+//     guss=prompt("guss again");
+
+// }
+//  else{
+//     console.log("you quit");
+//     // break;
+// }}
+
+
+// let fruits=["apple","banana","grapes"];
+
+// arr=[1,2,3,4,5,6];
+// let num=arr.length;
+// for(i=0;i<=arr.length;i++){
+//     console.log(arr[i])
+
+// }
+
+// ---------------------todo app...............
+// let todo=[];
+// let req=prompt("please enter your request");
+// while(true){
+//     if(req=="quit"){
+//         console.log("quitting app");
+//         break;
+//     }
+//     if(req=="list"){
+//         console.log("------------");
+//         for(i=0;i<=todo.length;i++){
+//             console.log(i,todo[i]);
+//             console.log("------------");
+//         }
+//     }
+//     else if(req=="add"){
+//         let task=prompt("please enter what to add ");
+//         todo.push(task);
+//         console.log("Task added");
+//     }
+//     else if(req=="delete"){
+//         let idx=prompt("what index you need to delete");
+//          todo.splice(idx,1);
+//          console.log("task deleted");      
+//     }
+//     else{
+//         console.log("Wrong Request");
+//     }
+//     req=prompt("please enter your request");
+// }
+
+// -------------------------------------------
+
+// const ran=prompt("ask the limit");
+
+// const  random=Math.floor(Math.random()*ran)+1;
+
+// const guss=prompt("enter your guss");
+
+// while(true){
+//     if(guss=="quit"){
+//         console.log("your quiting");
+//         break;
+//     }
+
+//     if(guss==random){
+//         console.log("you guss is right");
+//         break;
+//     }
+//     else{
+//         console.log("wring guss");
+//     }
+//     guss = prompt("guss was wrong retry");
+// }
+
+// -------------methods-------------------------
+
+// const calculator={
+//     add:function(a,b){
+//         return a+b;
+//     },
+//     sub:function(a,b){
+//         return a-b;
+//     },
+//     mul:function(a,b){
+//         return a*b;
+//     }
+//     }
+// -------------------------
+
+// -----------this function-----------------------
+//     const students={
+//         name:"farooq",
+//         age:27,
+//         eng:40,
+//         math:45,
+//         phy:40,
+//         getavg(){
+//             let avg=(this.eng+this.math+this.phy)/3;
+//             console.log(avg);
+
+//         } 
+// }
+
+// ---------------------------------------------
+
+
+// const sum=(a,b)=>{
+//     console.log(a+b);
+// }
+
+
+// sum(1,2);
+
+
+// const cube=(n)=>{
+//     return n*n*n;
+// }
+
+
+// const div=(a,b)=>(
+//     a/b
+// );
+
+// div(10,5);
+
+
+// console.log("farooq");
+
+// setInterval(() => {
+// console.log("hello farooq");},4000);
+// console.log("hello moto");
+
+
+// let id=setInterval(()=>{
+//     console.log("hello farooq");},2000);
+//     console.log(id);
+
+
+
+//     let square=(n)=>{
+
+//         return n*n;
+//     };
+
+//     console.log(square(4));
+
+
+
+//     let arr=[1,2,3,4,5];
+
+//     const arrayavg=(arr)=>{
+//         let total=0;
+//         for(array of arr){
+//             total=total+array;
+//         }
+//         return total/arr.length
+//     };
+
+//     console.log(arrayavg(arr));
+
+
+//     let iseven=(iseven)=>{
+//         if(iseven%2==0){
+//             return "even";
+//         }
+//         else{
+//             return "odd"
+//         }
+//     }
+
+//     console.log(iseven(17));
+// ------------------------------------
+//  function name(){
+//     console.log("farooq");
+// }
+
+
+
+// function loops(){
+//     for(i=0;i<=5;i++){
+//         console.log(i);
+//     }
+// }
+
+// function age(){
+
+//     if(age>=18){
+//         console.log("adult");
+//     }
+// else{
+//     console.log("not adult");
+// }};
+
+
+
+// function sum(a,b){
+//     console.log(a*b);
+// }
+
+// function sum(a,b){
+//     return a+b;
+// }
+
+
+// function allsum(n){
+//     let sum=0;
+//     for(i=1;i<=n;i++){
+//         sum=sum+i;
+//     }
+//     return sum;
+// }
+
+// function name(a,b){
+//     console.log("farooq"+(a+b));
+// }
+
+// name(1,2);
+
+
+// let sum=20;
+// function calsum(a,b){
+//     let sum=a+b;
+//     console.log(sum);
+// }
+// console.log(sum);
+
+// calsum(1,2);
+
+function outer() {
+    let x = 5;
+    let y = 10;
+
+    function inner() {
+        console.log(x);
+
+    }
+
+    inner();
+}
+
+
+const sum = function (a, b) {
+    return a + b;
+}
+
+
+let arr = [8, 9, 10, 1, 2, 3, 4, 5, 6, 7];
+
+function getElements(arr, num){
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > num) {
+            console.log(arr[i]);
+        }
     }
 }
+getElements(arr, 5);
